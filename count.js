70f0a6a1f71b121ff05b57250a28552c3b178ec8
boxuan <boxuan@ualberta.ca>
2018-01-17 20:16:41 +0000

    newthing

--- count.js
+++ count.js
@@ -1,19 +1,19 @@
-function aaa(word) {
-	word = word.toLowerCase();
-	var dictionary = {};
-	for ( var i = 0; i < word.length; i++ ) {
-		var c = word.charAt(i);
-		if (c == " ") { continue; }
-		if (c in dictionary ) { dictionary[c] = dictionary[c]+1;} 
-		else {
-			dictionary[c] = 1;}
-		}
-		return dictionary;}
-		console.log(aaa("lighthouse"));
-		
-
-
-
-
-
-
+function wordcount(word) {
+  word = word.toLowerCase();
+  var dictionary = {};
+  for (var i = 0; i < word.length; i++) {
+    var c = word.charAt(i);
+    if (c == " ") {
+      continue;
+    }
+    if (c in dictionary) {
+      
+      dictionary[c] = dictionary[c] + 1;
+    } else {
+      
+      dictionary[c] = 1;
+    }
+  }
+  return dictionary;
+}
+console.log(wordcount("lighthouse dasdsa"));
---
+++ test.js
@@ -0,0 +1,7 @@
+var object1 = { name : ["tom" , "tom3" ]};
+object1["age"]= [];
+object1.age.push("27");
+object1.age.push("35");
+object1.name.push("tom2") ;
+
+console.log((object1));
